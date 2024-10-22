369e2ca2b40691aa4cb08867b77697ff8719252e
Yeongi Park <yeongi0111@naver.com>
2024-10-23 08:38:54 +0900

    feat(validationUtil): validation 함수 구현
    테스트 코드 먼저 작성 후, 함수 구현
    유효성 검사에 필요한 함수를 validationUtil.js 파일에 작성하여, 함수의
    모듈화 진행

---
+++ __tests__/ValidationUtilTest.js
@@ -0,0 +1,47 @@
+import { ERROR } from '../constant';
+import {
+  checkEmptyString,
+  checkMinCarNumber,
+  checkMaxCarNumber,
+  checkMinTryNumber,
+  checkMaxTryNumber,
+} from '../src/util/validationUtil';
+
+describe('validateUtil 함수 테스트', () => {
+  test('빈 문자열 입력시 예외 처리', () => {
+    const emptyInput = '';
+    const whitespaceInput = '   ';
+    expect(() => checkEmptyString(emptyInput)).toThrow(ERROR.EMPTY_STRING);
+    expect(() => checkEmptyString(whitespaceInput)).toThrow(ERROR.EMPTY_STRING);
+  });
+
+  test('자동차 이름을 1개 이하로 입력시 예외 처리', () => {
+    const input = ['pobi'];
+
+    expect(() => checkMinCarNumber(input)).toThrow(ERROR.MIN_CAR_NUMBER);
+  });
+
+  test('자동차 이름 21개 이상 입력시 예외 처리', () => {
+    const input = Array(21).fill('carName');
+
+    expect(() => checkMaxCarNumber(input)).toThrow(ERROR.MAX_CAR_NUMBER);
+  });
+
+  test('시도 횟수 1이하인 경우 예외 처리', () => {
+    const negativeInput = '-1';
+    const zeroInput = '0';
+
+    expect(() => checkMinTryNumber(negativeInput)).toThrow(
+      ERROR.MIN_TRY_NUMBER,
+    );
+    expect(() => checkMinTryNumber(zeroInput)).toThrow(ERROR.MIN_TRY_NUMBER);
+  });
+
+  test(`시도 횟수 10001번 이상인 경우 예외 처리`, () => {
+    const failInput = '10001';
+    const trueInput = '10000';
+
+    expect(() => checkMaxTryNumber(trueInput)).not.toThrow();
+    expect(() => checkMaxTryNumber(failInput)).toThrow(ERROR.MAX_TRY_NUMBER);
+  });
+});
---
+++ src/util/validationUtil.js
@@ -0,0 +1,31 @@
+import { ERROR } from '../../constant';
+
+export function checkEmptyString(input) {
+  if (input.trim() === '') {
+    throw new Error(ERROR.EMPTY_STRING);
+  }
+}
+
+export function checkMinCarNumber(cars) {
+  if (cars.length < 2) {
+    throw new Error(ERROR.MIN_CAR_NUMBER);
+  }
+}
+
+export function checkMaxCarNumber(cars) {
+  if (cars.length > 20) {
+    throw new Error(ERROR.MAX_CAR_NUMBER);
+  }
+}
+
+export function checkMinTryNumber(tryNumber) {
+  if (Number(tryNumber) < 1) {
+    throw new Error(ERROR.MIN_TRY_NUMBER);
+  }
+}
+
+export function checkMaxTryNumber(tryNumber) {
+  if (Number(tryNumber) > 10000) {
+    throw new Error(ERROR.MAX_TRY_NUMBER);
+  }
+}
