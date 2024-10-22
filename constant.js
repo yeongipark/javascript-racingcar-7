90bb1cc3e31f8d10860d91351f542c9c2c5a327c
Yeongi Park <yeongi0111@naver.com>
2024-10-23 08:05:10 +0900

    feat(constant.js): 어플리케이션에서 필요한 상수 정리
    constant.js에 어플리케이션에서  필요한 상수를 정리하여, 여러 곳에서
    사용되는 상수를 하나의 파일에서 관리될 수 있도록 함

---
+++ constant.js
@@ -0,0 +1,19 @@
+export const ERROR = {
+  EMPTY_STRING: '[ERROR] 자동차 이름을 입력해주세요',
+  MIN_CAR_NUMBER: '[ERROR] 자동차 이름은 최소 2개 이상 입력해 주세요',
+  MAX_CAR_NUMBER: '[ERROR] 자동차의 개수는 20개 이하로 입력해 주세요',
+  MIN_TRY_NUMBER: '[ERROR] 시도 횟수는 1회 이상 입력해 주세요',
+  MAX_TRY_NUMBER: '[ERROR] 시도 횟수는 10000회 이하로 입력해 주세요',
+};
+
+export const INPUT = {
+  CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  TRY_NUMBER: '시도할 횟수는 몇 회인가요?',
+};
+
+export const OUTPUT = {
+  EXCUTION_RESULT: '실행 결과',
+  WINNERS: '최종 우승자 : ',
+};
+
+export const DASH = '-';
