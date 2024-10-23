a2acd5c425b6694c5f0c37225dbe93a7196212f2
Yeongi Park <yeongi0111@naver.com>
2024-10-23 22:02:39 +0900

    feat(App.js): App.js의 run 메서드 작성
    전체 프로그램 실행을 담당하는 App.js에서 의존성 주입을 사용하기 위해,
    inputView, outputView, racingCarService를 만들어 racingCarController로
    전달함
    또한 run메서드에서 racingCarController의 run 메서드를 호출하여
    프로그램을 동작시킴

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import RacingCarController from './controller/RacingCarController.js';
+import RacingCarService from './service/RacingCarService.js';
+import { InputView } from './view/InputView.js';
+import { OutputView } from './view/OutputView.js';
+
 class App {
-  async run() {}
+  #racingCarController;
+  constructor() {
+    const inputView = new InputView();
+    const outputView = new OutputView();
+    const racingCarService = new RacingCarService();
+    this.#racingCarController = new RacingCarController(
+      racingCarService,
+      inputView,
+      outputView,
+    );
+  }
+
+  async run() {
+    await this.#racingCarController.run();
+  }
 }
 
 export default App;
