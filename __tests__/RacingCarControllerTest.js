0d0a888f3a3317882b6e1460ea441cfc148125b8
Yeongi Park <yeongi0111@naver.com>
2024-10-23 11:26:45 +0900

    feat(RacingCarController): RacingCarController 클래스 구현
    전체적인 게임의 흐름을 담당하는 RacingCarController 클래스 구현

---
+++ __tests__/RacingCarControllerTest.js
@@ -0,0 +1,59 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import RacingCarService from '../src/service/RacingCarService.js';
+import { InputView } from '../src/view/InputView.js';
+import { OutputView } from '../src/view/OutputView.js';
+import RacingCarController from '../src/controller/RacingCarController.js';
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('RacingCarController 클래스 테스트', () => {
+  test('실제 어플리케이션 동작 테스트', async () => {
+    const inputs = ['pobi,woni,yeongi', 2];
+    const randomValue = [5, 1, 7, 4, 4, 6];
+    const logs = [
+      'pobi : -',
+      'woni : ',
+      'yeongi : -',
+      'pobi : --',
+      'woni : -',
+      'yeongi : --',
+      '최종 우승자 : pobi, yeongi',
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms(randomValue);
+
+    const racingCarController = new RacingCarController(
+      new RacingCarService(),
+      new InputView(),
+      new OutputView(),
+    );
+    await racingCarController.run();
+
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
+});
---
+++ src/controller/RacingCarController.js
@@ -0,0 +1,35 @@
+import { OUTPUT } from '../../constant';
+import Validator from '../Validator';
+
+export default class RacingCarController {
+  #racingCarService;
+  #inputView;
+  #ouputView;
+
+  constructor(racingCarService, inputView, outputView) {
+    this.#racingCarService = racingCarService;
+    this.#inputView = inputView;
+    this.#ouputView = outputView;
+  }
+
+  async run() {
+    const carNamesString = await this.#inputView.inputCarNames();
+    Validator.validateUserInput(carNamesString);
+    this.#racingCarService.saveCars(carNamesString);
+
+    const tryNumber = await this.#inputView.inputTryNumber();
+    Validator.validateTryNumber(tryNumber);
+
+    this.#ouputView.printString(OUTPUT.EXCUTION_RESULT);
+    Array(tryNumber)
+      .fill(0)
+      .forEach(() => {
+        const advancedResult = this.#racingCarService.processCarMovement();
+        this.#ouputView.printAdvancedResult(advancedResult);
+        this.#ouputView.printEmptyLine();
+      });
+
+    const winners = this.#racingCarService.getWinners();
+    this.#ouputView.printWinners(winners);
+  }
+}
